93a84e760ab4b99b7068fa7d3961f55a2a82d928
Arnel Jan Sarmiento <rneljan@gmail.com>
2023-05-10 22:56:04 +0800

    Added gitignore and edited explanations

--- Bayron_Sarmiento_PE7/Main.java
+++ Bayron_Sarmiento_PE7/Main.java
@@ -1,3 +1,11 @@
+//  Name: Sean Gabriel Bayron
+//  Student Number: 2021-04354
+//  Name: Arnel Jan Sarmiento
+//  Student Number: 2021-05094
+//  Date: 2023-4-30
+//  This program is a Java Program that demonstrates the use of Inheritance, Encapsulation, Method Overloading and Method Overriding.
+//  The program was developed by Sean Gabriel Bayron and Arnel Jan Sarmiento.
+
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle("red", 5.0);
